3dde839f2364cc12f9ebeb5d5a9da69730407c3a
nabilahdyh <nabilahidayah.nh@gmail.com>
2022-06-01 23:44:07 +0700

    input the necessary variable

--- index.js
+++ index.js
@@ -1,20 +1,84 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const favColorMonica = Array.from(new Set(["Yellow", "Pink", "White", "Purple"]));
+const favRestaurantMonica = Array.from(new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]));
+
+const favColorWendy = Array.from(new Set(["Blue", "Black", "Grey"]));
+const favRestaurantWendy = Array.from(new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]));
+
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: favColorMonica,
+  isHavePet: "Yes",
+  education: [
+    {
+      name: "SD 01",
+      city: "Jakarta",
+      graduate: 2016,
+    },
+    {
+      name: "SMP 02",
+      city: "Jakarta",
+      graduate: 2019,
+    },
+    {
+      name: "SMA 03",
+      city: "Tangerang",
+      graduate: null,
+    },
+  ],
+  favoriteRestaurant: favRestaurantMonica,
+};
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 17,
+  email: "wendy@dingdong.com",
+  favoriteColor: favColorWendy,
+  isHavePet: "No",
+  education: [
+    {
+      name: "SD 02",
+      city: "Jakarta",
+      graduate: "2010",
+    },
+    {
+      name: "SMP 03",
+      city: "Bogor",
+      graduate: "2013",
+    },
+    {
+      name: "SMA 01",
+      city: "Surabaya",
+      graduate: "2016",
+    },
+    {
+      name: "Universitas Maju",
+      city: "Tangerang",
+      graduate: "0",
+    },
+  ],
+  favoriteRestaurant: favRestaurantWendy,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
